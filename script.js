08a6781179feb4c19e6d2e1f36a4371c4c6dd4cf
oliver-turner <oliver01turner@gmail.com>
2022-07-08 00:28:38 +1000

    nav and landing page css reformatting, animation for modal pop up added

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function toggleBurger() {
     let burgerItems = document.querySelectorAll(".hamburgerMenu a");
     toggleMenu.addEventListener('change', function () {
         if (toggleMenu.checked) {
-            burgerItems[0].style.transform = "translate(0, 100px)";
-            burgerItems[1].style.transform = "translate(-95px, 0px)";
-            burgerItems[2].style.transform = "translate(-90px, 50px)";
-            burgerItems[3].style.transform = "translate(-50px, 85px)";
+            burgerItems[0].style.transform = "translate(-95px, 0px)";
+            burgerItems[1].style.transform = "translate(-90px, 50px)";
+            burgerItems[2].style.transform = "translate(-50px, 85px)";
+            burgerItems[3].style.transform = "translate(0, 100px)";
         }
         else {
             burgerItems.forEach((menuItem) => {
