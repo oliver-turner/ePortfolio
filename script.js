8207f49efba31db025e140bf4b8d91124aede044
Oliver Turner <oliver01turner@gmail.com>
2022-07-12 19:38:03 +1000

    reordering of burger menu items in hamburger

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function toggleBurger() {
     let burgerItems = document.querySelectorAll(".hamburgerMenu a");
     toggleMenu.addEventListener('change', function () {
         if (toggleMenu.checked) {
-            burgerItems[0].style.transform = "translate(-95px, 0px)";
-            burgerItems[1].style.transform = "translate(-90px, 50px)";
+            burgerItems[0].style.transform = "translate(-90px, 50px)";
+            burgerItems[1].style.transform = "translate(-95px, 0px)";
             burgerItems[2].style.transform = "translate(-50px, 85px)";
             burgerItems[3].style.transform = "translate(0, 100px)";
         }
